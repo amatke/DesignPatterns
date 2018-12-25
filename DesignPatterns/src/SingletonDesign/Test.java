6a8e16b5321d6ecab5ff64c67e17310cb5c7b036
amatke <lexamatovic@gmail.com>
2018-12-25 22:56:49 +0100

    Observer pattern

--- DesignPatterns/src/SingletonDesign/Test.java
+++ DesignPatterns/src/SingletonDesign/Test.java
@@ -10,3 +10,11 @@ public class Test {
 	}
 
 }
+
+/*Obicno se koristi kod pools, zatim konekcija, ili threadPools.
+Greske se javljaju jer postoje u nekim programima vise kopija nekih objekata 
+za koje treba postojati samo jedan objekat.
+Jedinstvena je instanca jer se moze kreirati samo iz sebe = private konstruktor.
+Instancira se samo pozivom klase i njene staticne metode u kojoj se poziva taj 
+privatni konstruktor.
+To je sve!*/
